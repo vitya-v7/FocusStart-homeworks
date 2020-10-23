fc818702eccfbde768c9bef31b6f8f512afcb319
Admin <>
2020-10-23 16:00:08 +0300

    Revert "added folder"
    This reverts commit 03143e32bb9cf5da7128fcea48ebb978d985fe10.

---
+++ TestTaskCFT/CarDetailModule/Presenter/CarDetailPresenter.swift
@@ -0,0 +1,109 @@
+//
+//  CarDetailPresenter.swift
+//  TestTaskCFT
+//
+//  Created by Viktor D. on 18.09.2020.
+//  Copyright © 2020 Viktor D. All rights reserved.
+//
+
+import UIKit
+
+class CarDetailPresenter {
+    var key: String?
+    var carService: IDetailsCarServiceInterface?
+    var view: ICarDetailViewInput?
+    var carModel: CarModel?
+
+    func convertCarModelToViewModel(car: CarModel) -> CarDetailViewModel {
+        let viewModel = CarDetailViewModel(withElementModel: car)
+        return viewModel
+    }
+}
+
+extension CarDetailPresenter: ICarDetailViewOutput
+{
+	func callPopover(fromView view: UIView, option: String) {
+		let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+		let pv = storyboard.instantiateViewController(withIdentifier: "PickerViewIdentifier") as! PickerView
+		pv.output = self
+		pv.currentOption = option
+		switch view.tag {
+			case 1:
+				pv.type = .carModel
+			case 2:
+				pv.type = .carBodyStyle
+			case 4:
+				pv.type = .carCountry
+			default:
+				pv.type = .carYear
+		}
+
+		pv.modalPresentationStyle = UIModalPresentationStyle.popover
+			   pv.preferredContentSize = CGSize(width: 300, height: 300)
+		pv.picker?.backgroundColor = UIColor.white
+		pv.output = self
+		self.view?.present(pv, animated: true, completion: nil)
+		let popover = pv.popoverPresentationController
+		popover?.permittedArrowDirections = .any
+		popover?.sourceView = view
+		popover?.sourceRect = (view.bounds)
+	}
+
+	func changeSelectedDataInView(type: PickerType, index: Int) {
+		switch type {
+			case .carModel:
+					carModel?.model = CarService.CarModels.allCases[index]
+			case .carBodyStyle:
+				   carModel?.body = CarService.CarBodyStyle.allCases[index]
+			case .carCountry:
+				   carModel?.manufacturer = CarService.CarCountry.allCases[index]
+			default: print("unknown option")
+		}
+		reloadData()
+	}
+
+	func viewDidLoadDone() {
+		view?.setInitialState()
+		if key != nil {
+			carModel = carService?.getCar(key: key!)
+		}
+		else {
+			carModel = CarModel.init(model: CarService.CarModels.allCases[0], manufacturer: CarService.CarCountry.allCases[0], body: CarService.CarBodyStyle.allCases[0], carKey: nil)
+		}
+		reloadData()
+	}
+
+	func saveCarInDB() {
+		if carModel!.carKey == nil {
+			carService?.addCar(car: carModel!)
+		}
+		else {
+			carService?.updateCar(car: carModel!)
+		}
+		self.view?.navigationController?.popViewController(animated: true)
+	}
+
+	func saveSelectedTextFieldValue(type: PickerType, value: String) {
+		switch type {
+			case .carYear:
+				   if value.trimmingCharacters(in: .whitespaces).isEmpty {
+					   carModel?.yearOfIssue = nil
+				   }
+				   else {
+					   carModel?.yearOfIssue = Int(value)
+				   }
+			case .carNumber:
+				   if value.trimmingCharacters(in: .whitespaces).isEmpty {
+					   carModel?.carNumber = nil
+				   }
+				   else {
+					   carModel?.carNumber = value
+				   }
+			default: print("unknown option")
+		}
+	}
+
+	func reloadData() {
+		view!.setViewModel(viewModel: CarDetailViewModel.init(withElementModel: carModel!))
+	}
+}
---
+++ TestTaskCFT/CarDetailModule/View/CarDetailView.swift
@@ -0,0 +1,90 @@
+//
+//  CarDetailView.swift
+//  TestTaskCFT
+//
+//  Created by Viktor D. on 18.09.2020.
+//  Copyright © 2020 Viktor D. All rights reserved.
+//
+
+import UIKit
+
+protocol ICarDetailViewInput : UIViewController  {
+    func setInitialState()
+    func setViewModel(viewModel: CarDetailViewModel)
+}
+
+protocol ICarDetailViewOutput {
+    func callPopover(fromView: UIView, option: String)
+	func changeSelectedDataInView(type: PickerType, index: Int)
+	func saveSelectedTextFieldValue(type: PickerType, value: String)
+    func viewDidLoadDone()
+    func saveCarInDB()
+	func reloadData()
+}
+
+class CarDetailView: UIViewController {
+    
+    var output: ICarDetailViewOutput?
+    var viewModel: CarDetailViewModel?
+    
+    @IBOutlet weak var carModel: UITextField!
+    @IBOutlet weak var carYear: UITextField!
+    @IBOutlet weak var carCountry: UITextField!
+    @IBOutlet weak var carBodyStyle: UITextField!
+	@IBOutlet weak var carNumberLabel: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        output?.viewDidLoadDone()
+    }
+    
+    @objc func saveCar(_ sender: Any) {
+		output?.saveSelectedTextFieldValue(type: .carNumber, value: carNumberLabel.text!)
+		if ((Int(carYear.text!)) != nil) ||
+			(carYear.text!.trimmingCharacters(in: .whitespaces).isEmpty)  {
+			output?.saveSelectedTextFieldValue(type: .carYear, value: carYear.text!)
+		}
+		output?.reloadData()
+        output?.saveCarInDB()
+    }
+}
+
+extension CarDetailView: UITextFieldDelegate {
+	func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+		if output != nil && textField.tag != 3 && textField.tag != 6 {
+			output?.callPopover(fromView: textField, option: textField.text!)
+			return false
+		}
+		return true
+	}
+}
+
+extension CarDetailView: ICarDetailViewInput
+{
+	func setInitialState() {
+	   let rightButton = UIBarButtonItem.init(barButtonSystemItem: .save, target: self, action: #selector(saveCar(_:)))
+	   self.navigationItem.rightBarButtonItem = rightButton
+	   carModel.delegate = self
+	   carCountry.delegate = self
+	   carBodyStyle.delegate = self
+	   carYear.delegate = self
+		carNumberLabel.delegate = self
+
+	}
+
+	func setViewModel(viewModel: CarDetailViewModel) {
+		self.viewModel = viewModel
+		self.configureDetailView(withObject: viewModel)
+	}
+}
+
+private extension CarDetailView
+{
+	private func configureDetailView(withObject object: CarDetailViewModel) {
+		self.carModel!.text = object.carModel
+		self.carCountry!.text = object.carCountry
+		self.carBodyStyle!.text = object.carBodyStyle
+		self.carNumberLabel.text = object.carNumber
+		self.carYear.text = object.carYear
+	}
+}
