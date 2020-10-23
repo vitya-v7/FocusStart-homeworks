18214bcd83631c3c9219738ca327594ea3f8e487
Admin <>
2020-10-23 15:44:00 +0300

    Revert "добавлена вложенная папка"
    This reverts commit 7b893aaa05f9ca7d7b2d65fd7073f4250c1bb541.

---
+++ TestTaskCFT/CarsListModule/View/Cell/ViewModel/CarsElementViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  CarsElementViewModel.swift
+//  TestTaskCFT
+//
+//  Created by Viktor D. on 16.09.2020.
+//  Copyright © 2020 Viktor D. All rights reserved.
+//
+
+import UIKit
+
+struct CarsElementViewModel {
+    
+    var carModel: String?
+    var carYear: String?
+    var carCountry: String?
+    var carBodyStyle: String?
+	var carNumber: String?
+
+    init (withElementModel model: CarModel) {
+		self.carModel = model.model.rawValue
+		self.carCountry = model.manufacturer.rawValue
+		self.carBodyStyle = model.body.rawValue
+		if let year = model.yearOfIssue {
+			carYear = String(year)
+		}
+		if let number = model.carNumber {
+			carNumber = String(number)
+		}
+        
+    }
+}
---
+++ TestTaskCFT/Services/Data/CarsDataService/Models/CarModel.swift
@@ -0,0 +1,18 @@
+//
+//  CarModel.swift
+//  TestTaskCFT
+//
+//  Created by Viktor D. on 16.09.2020.
+//  Copyright © 2020 Viktor D. All rights reserved.
+//
+
+import UIKit
+
+struct CarModel: Codable {
+	var model: CarService.CarModels
+	var manufacturer: CarService.CarCountry
+    var yearOfIssue: Int?
+	var body: CarService.CarBodyStyle
+	var carNumber: String?
+    var carKey: String?
+}
