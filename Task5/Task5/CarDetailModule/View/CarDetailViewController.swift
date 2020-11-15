2e904b583f540903d790d19ede505706b11d1a5d
Admin <admin@Mac-Admin.local>
2020-11-15 23:43:45 +0300

    стартовая точка

---
+++ Task5/Task5/CarDetailModule/View/CarDetailViewController.swift
@@ -0,0 +1,99 @@
+//
+//  CarDetailViewController.swift
+//  Task5
+//
+//  Created by Viktor D. on 18.09.2020.
+//  Copyright © 2020 Viktor D. All rights reserved.
+//
+
+import UIKit
+
+protocol ICarDetailViewInput : UIViewController  {
+	func setInitialState()
+	func setViewModel(viewModel: CarDetailViewModel)
+}
+
+protocol ICarDetailViewOutput {
+	func callPopover(fromView: UIView, option: String)
+	func changeSelectedDataInView(type: PickerType, index: Int)
+	func saveSelectedTextFieldValue(type: PickerType, value: String)
+	func viewDidLoadDone()
+	func saveCarInDB()
+	func reloadData()
+}
+
+class CarDetailViewController: UIViewController {
+
+	var output: ICarDetailViewOutput?
+	var viewModel: CarDetailViewModel?
+	enum textFieldsWithTags: Int {
+		case carModel = 1
+		case carBodyStyle
+		case carYear
+		case carCountry
+		case carNumber
+	}
+
+	@IBOutlet weak var carModel: UITextField!
+	@IBOutlet weak var carYear: UITextField!
+	@IBOutlet weak var carCountry: UITextField!
+	@IBOutlet weak var carBodyStyle: UITextField!
+	@IBOutlet weak var carNumberLabel: UITextField!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		output?.viewDidLoadDone()
+	}
+
+	@objc func saveCar(_ sender: Any) {
+		output?.saveSelectedTextFieldValue(type: .carNumber, value: carNumberLabel.text ?? "")
+		guard let carYearLabel = carYear.text else {
+			return
+		}
+		if ((Int(carYearLabel)) != nil) ||
+			(carYearLabel.trimmingCharacters(in: .whitespaces).isEmpty)  {
+			output?.saveSelectedTextFieldValue(type: .carYear, value: carYearLabel)
+		}
+		output?.reloadData()
+		output?.saveCarInDB()
+	}
+}
+
+extension CarDetailViewController: UITextFieldDelegate {
+	func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+		if output != nil && textField.tag != textFieldsWithTags.carYear.rawValue && textField.tag != textFieldsWithTags.carNumber.rawValue {
+			output?.callPopover(fromView: textField, option: textField.text ?? "")
+			return false
+		}
+		return true
+	}
+}
+
+extension CarDetailViewController: ICarDetailViewInput
+{
+	func setInitialState() {
+		let rightButton = UIBarButtonItem.init(barButtonSystemItem: .save, target: self, action: #selector(saveCar(_:)))
+		self.navigationItem.rightBarButtonItem = rightButton
+		carModel.delegate = self
+		carCountry.delegate = self
+		carBodyStyle.delegate = self
+		carYear.delegate = self
+		carNumberLabel.delegate = self
+	}
+
+	func setViewModel(viewModel: CarDetailViewModel) {
+		self.viewModel = viewModel
+		self.configureDetailView(withObject: viewModel)
+	}
+}
+
+private extension CarDetailViewController
+{
+	func configureDetailView(withObject object: CarDetailViewModel) {
+		self.carModel.text = object.carModel
+		self.carCountry.text = object.carCountry
+		self.carBodyStyle.text = object.carBodyStyle
+		self.carNumberLabel.text = object.carNumber
+		self.carYear.text = object.carYear
+	}
+}
---
+++ Task5/Task5/CarsListModule/View/Cell/CarsElementCell.swift
@@ -0,0 +1,50 @@
+//
+//  CarsElementCell.swift
+//  Task5
+//
+//  Created by Viktor D. on 17.09.2020.
+//  Copyright © 2020 Viktor D. All rights reserved.
+//
+
+import UIKit
+
+class CarsElementCell: UITableViewCell {
+	var viewModel: CarsElementViewModel?
+	
+	@IBOutlet weak var carModel: UILabel!
+	@IBOutlet weak var carYear: UILabel!
+	@IBOutlet weak var carCountry: UILabel!
+	@IBOutlet weak var carBodyStyle: UILabel!
+	@IBOutlet weak var carNumber: UILabel!
+	
+	
+	static let reuseIdentifier = "CarsListCellIdentifier"
+	
+	func configureCell(withObject object: CarsElementViewModel?) {
+		viewModel = object
+
+		guard let object = object, let carModel = object.carModel, let carBodyStyle = object.carBodyStyle, let carCountry = object.carCountry else {
+			return
+		}
+		self.carModel.text = "Model: " + carModel
+		self.carBodyStyle.text = "Body Style: " + carBodyStyle
+		self.carCountry.text = "Country: " + carCountry
+		if let year = object.carYear {
+			self.carYear.text = "Year: " + year
+		}
+		else {
+			self.carYear.text = "Year: -"
+		}
+		if let number = object.carNumber {
+			self.carNumber.text = "Car number: " + number
+		}
+	}
+	
+	override func prepareForReuse() {
+		self.carModel.text = nil
+		self.carYear.text = nil
+		self.carBodyStyle.text = nil
+		self.carCountry.text = nil
+		self.carNumber.text = nil
+	}
+}
