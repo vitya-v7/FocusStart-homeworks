b56d4f52a0de680f2821458040dd2c97cf02613f
user183410 <user183410@FF049.local>
2020-11-23 12:22:47 +0100

    создал ветку с дз 6

---
+++ Task6/Task6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task6
+//
+//  Created by user183410 on 11/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
