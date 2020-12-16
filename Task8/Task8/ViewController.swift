7424db4a18a5d374f6804aceb2490787fa9fb431
Admin <admin@Mac-Admin.local>
2020-12-16 17:37:11 +0300

    начал

---
+++ Task8/Task8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task8
+//
+//  Created by Admin on 16.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
