802659ccf3dac9059cbf3c9f63319b8e46c9f206
Admin <admin@Mac-Admin.local>
2020-12-17 01:38:48 +0300

    урок

--- Task8/Task8/ViewController.swift
+++ Task8/Task8/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+	@IBOutlet weak var tableView: UITableView!
+
+	let appDelegate = UIApplication.shared.delegate as! AppDelegate
+	var context: NSManagedObjectContext!
+	var container: NSPersistentContainter!
+
+	var lists = [List]() {
+		didSet {
+			self.tableView.reloadData()
+		}
 	}
 
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		container = self.appDelegate.persistentContainer
+		self.context = self.container.viewContext
+		self.fetchData()
+	}
+
+	func fetchData() {
+		let request: NSFetchRequest<List> = List.fetchRequest()
+		do {
+			lists = try context.fetch(request)
+		}
+		catch {
+			print(error)
+		}
+	}
+}
+
+extension ViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return self.lists.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath)
+		let list = lists[indexPath.row]
+		cell.textLabel?.text = list.name
+		return cell
+	}
+}
+
+extension ViewController: UITableViewDelegate {
 
 }
 
+extension ViewController: AddListViewControllerDelegate {
+	func addList(name:String) {
+		let list = List(context:  self.context)
+		list.name = name //здесь уже происходит добавление в б/д, на след строке просто сохраняем контекст
+		appDelegate.saveContext()
+		fetchData()
+	}
+}
