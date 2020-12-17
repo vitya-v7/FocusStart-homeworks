6ba4ef8993bc63b7279ff877b487347a28e739b0
Admin <admin@Mac-Admin.local>
2020-12-17 04:32:56 +0300

    функция добавления человека

--- Task8/Task8/CoreData/CoreDataService.swift
+++ Task8/Task8/CoreData/CoreDataService.swift
@@ -11,13 +11,13 @@ import CoreData
 
 struct CoreDataService {
 
-	static let shared = CoreDataManager()
+	static let shared = CoreDataService()
 
 	private let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
 	func addCompany(_ name: String) -> Company? {
 		guard let entityDescription = NSEntityDescription.entity(forEntityName: "Company", in: managedContext)
-			else { return nil }
+		else { return nil }
 		let company = NSManagedObject(entity: entityDescription, insertInto: managedContext) as! Company
 
 		company.name = name
@@ -30,4 +30,43 @@ struct CoreDataService {
 			return nil
 		}
 	}
+
+	func addPerson(age: Int,
+				   name: String,
+				   position: String,
+				   experience: Int?,
+				   education: String?,
+				   company: Company) {
+
+		guard let entityDescription = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)
+		else { return }
+
+		let personEntity = NSManagedObject(entity: entityDescription, insertInto: managedContext) as! Person
+
+		personEntity.name = name
+		personEntity.age = NSNumber.init(value: age)
+		personEntity.position = position
+		personEntity.toCompany = company
+
+		if experience != nil {
+		personEntity.experience = NSNumber.init(value: experience!)
+		}
+		else {
+			personEntity.experience = nil
+		}
+
+		if education != nil {
+		personEntity.education = education!
+		}
+		else {
+			personEntity.education = nil
+		}
+
+		do {
+			try managedContext.save()
+		} catch let error {
+			print(error.localizedDescription)
+		}
+	}
+
 }
